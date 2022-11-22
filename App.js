ca5903db3263ca3f54b9530a0e84eebe01b335bb
isouravganguly <isouravganguly@gmail.com>
2022-11-22 16:04:40 +0530

    master/first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TodoList from './src/screens/TodoList'
+
+const App = () => {
+  return (
+    <TodoList />
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
